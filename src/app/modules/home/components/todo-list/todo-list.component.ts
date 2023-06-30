692da0d8622634eddb637a5b042a107e048ba1a9
unknown <caiocbahia@hotmail.com>
2023-06-30 17:27:48 -0300

    build: added basic logic for list management

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
-
+  public taskList: Array<TaskList> = [
+    { task: 'Minha nova Task', checked: true },
+    { task: 'Minha segunda Task', checked: false },
+  ];
 }
