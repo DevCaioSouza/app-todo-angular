1c0ed3f40d35406e310ed54c2aca64b8fe8580f3
unknown <caiocbahia@hotmail.com>
2023-06-30 18:04:17 -0300

    build: added logic for  deleting one item and all items

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -13,4 +13,17 @@ export class TodoListComponent {
     { task: 'Minha nova Task', checked: true },
     { task: 'Minha segunda Task', checked: false },
   ];
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Deseja mesmo deletar todos os itens da lista?")
+
+    if(confirm){
+      this.taskList = [];
+    }
+
+  }
 }
